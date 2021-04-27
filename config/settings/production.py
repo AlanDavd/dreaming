8c3d2ed7b42d52c2374587274942d554521fca5d
AlanDavd <alandavidrl11@gmail.com>
2021-04-27 13:39:23 -0500

    add caddy

--- config/settings/production.py
+++ config/settings/production.py
@@ -5,7 +5,7 @@ from .base import env
 
 # Base
 SECRET_KEY = env('DJANGO_SECRET_KEY')
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['54.152.121.212'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=[''])
 
 # Databases
 DATABASES['default'] = env.db('DATABASE_URL')  # NOQA
